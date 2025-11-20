962266f72c373dfb52d0a7b67d662f58555dbe47
oangia <hqnhatdn@gmail.com>
2025-11-20 17:20:23 +0700

    Update ptaw.py

--- ptaw.py
+++ ptaw.py
@@ -6,11 +6,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST', 'OPTIONS'])
 def hello_world():
     result = handle_request(request)
-    response = jsonify(result)
+    data, status = result
+    response = jsonify(data)
+    response.status_code = status
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
-
+def handle(request):
+    if request.method == 'OPTIONS':
+        return ({}, 200)
+    return (dict(request.headers), 200)
 if __name__ == '__main__':
     app.run()

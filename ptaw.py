3b18b35c63759c367cc0123a77913cd7fd0b4ac6
oangia <hqnhatdn@gmail.com>
2025-11-21 13:37:06 +0700

    Update ptaw.py

--- ptaw.py
+++ ptaw.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST', 'OPTIONS'])
 def hello_world():
     data, status = handle_request(request)
-    response = jsonify(data)
-    return cors(response, status)
+    return cors(jsonify(data), status)
 
 if __name__ == '__main__':
     app.run()

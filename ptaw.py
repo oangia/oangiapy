f4881e330b46fed2c57f73ad0676f3af165305cc
oangia <hqnhatdn@gmail.com>
2025-11-21 13:36:34 +0700

    Update ptaw.py

--- ptaw.py
+++ ptaw.py
@@ -1,21 +1,13 @@
 from flask import Flask, request, jsonify
-from oangiapy.readability import handle_request
+from oangiapy.readability import handle_request, cors
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST', 'OPTIONS'])
 def hello_world():
-    result = handle_request(request)
-    data, status = result
+    data, status = handle_request(request)
     response = jsonify(data)
-    response.status_code = status
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    return response
-def handle(request):
-    if request.method == 'OPTIONS':
-        return ({}, 200)
-    return (dict(request.headers), 200)
+    return cors(response, status)
+
 if __name__ == '__main__':
     app.run()

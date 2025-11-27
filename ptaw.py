790ca3012d98a8f5c065ff6bf4bfca41b3191d20
oangia <hqnhatdn@gmail.com>
2025-11-27 16:23:28 +0700

    Update ptaw.py

--- ptaw.py
+++ ptaw.py
@@ -6,14 +6,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST', 'OPTIONS'])
 def hello_world():
-    adapter = FlaskAdapter(request)
-    # Handle OPTIONS pre-flight or invalid origin
-    if adapter.preflight():
-        return adapter.respPreflight()
-    # Pass adapted request to core
-    result, status = analyze(adapter)
-    # Convert core output to Flask response
-    return adapter.resp(result, status)
+    adapter = FlaskAdapter(request, handler=analyze)
+    return adapter.process()
 
 if __name__ == '__main__':
     app.run()

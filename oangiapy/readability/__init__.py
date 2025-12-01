ea3ce486af73b9b991a0d2e7dc025ad314b04f98
oangia <hqnhatdn@gmail.com>
2025-12-01 16:46:08 +0700

    Update __init__.py

--- oangiapy/readability/__init__.py
+++ oangiapy/readability/__init__.py
@@ -1,16 +1,1 @@
-from oangiapy.readability.ReadabilityEngine import ReadabilityEngine
-from oangiapy.web import FlaskAdapter 
-from oangiapy.crypto import Crypto
-    
-def readability(adapter, data):
-    ip = adapter.get_client_ip()
-    text = data.get("text")
-    pub_key = data.get("pub")
-    length = len(text)
-    if length < 100 or length > 1000:
-        return {'error': 'Input text must between 100 and 1000 characters long.'}, 400
-    
-    engine = ReadabilityEngine(text)
-    encrypted = Crypto.rsa_encrypt({"fomulas": engine.calculate()}, pub_key)
-    ip = Crypto.rsa_encrypt({"ip": ip}, pub_key)
-    return {"f": encrypted, "i": ip}, 200
+

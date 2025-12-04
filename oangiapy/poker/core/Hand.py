efcc7b2be8e93873bea5d18214b13f0612d5c25e
oangia <hqnhatdn@gmail.com>
2025-12-04 16:37:17 +0700

    Update Hand.py

--- oangiapy/poker/core/Hand.py
+++ oangiapy/poker/core/Hand.py
@@ -36,5 +36,5 @@ class Hand:
         return 0
 
     def __repr__(self):
-        return ",".join(c.get_name() for c in self._cards) + " " + self.get_type()
+        return ",".join(c.get_name() for c in self._cards) + " " + str(self.get_type())
       

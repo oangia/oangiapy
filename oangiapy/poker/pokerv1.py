86d4540aeb5ebae33f8dac9ce43fdb73c1b73b90
oangia <hqnhatdn@gmail.com>
2025-12-04 15:54:23 +0700

    Update pokerv1.py

--- oangiapy/poker/pokerv1.py
+++ oangiapy/poker/pokerv1.py
@@ -1,7 +1,6 @@
 from oangiapy.poker.core import Card as BaseCard
 from oangiapy.poker.core import HandDetector as BaseHandDetector
-from oangiapy.poker.core import HandType, PokerAlgo, Player
-
+from oangiapy.poker.core import HandType, Algorithm, Player
 class Card(BaseCard):
     def __init__(self, name):
         super().__init__(name)

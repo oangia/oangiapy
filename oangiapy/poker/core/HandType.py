bb33d27ac6fd48bcbe766bd260051b1cb1660978
oangia <hqnhatdn@gmail.com>
2025-12-04 15:42:58 +0700

    Create HandType.py

---
+++ oangiapy/poker/core/HandType.py
@@ -0,0 +1,11 @@
+class HandType:
+    ZITCH = 1
+    ONEPAIR = 2
+    TWOPAIR = 3
+    THREEKIND = 4
+    STRAIGHT = 5
+    FLUSH = 6
+    FULLHOUSE = 7
+    FOURKIND = 8
+    STRAIGHTFLUSH = 9
+      

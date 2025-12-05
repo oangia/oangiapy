8ef4c4a76045808f8f47a016b6415d8c8c3b160b
oangia <hqnhatdn@gmail.com>
2025-12-06 01:40:48 +0700

    Update Algorithm.py

--- oangiapy/poker/core/Algorithm.py
+++ oangiapy/poker/core/Algorithm.py
@@ -10,6 +10,21 @@ class Algorithm:
         self._cards = [self.get_card_class()(x) for x in cards.split(",")]
         self._cards.sort(key=lambda c: (c.get_rank(), c.get_suit()))
 
+    def split_5_5_3_index(self):
+        index = range(13)
+        res = []
+        for back in combinations(index, 5):
+            remain1 = [c for c in index if c not in back]
+            for middle in combinations(remain1, 5):
+                remain2 = [c for c in remain1 if c not in middle]
+                for front in combinations(remain2, 3):
+                    res.append((
+                        back, 
+                        middle, 
+                        front
+                    ))
+        return res
+        
     def get_card_class(self):
         return self._CardClass
 

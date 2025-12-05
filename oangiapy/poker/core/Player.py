fa334ecf8b5d5b7effd8db4b8fec5ea0503ca238
oangia <hqnhatdn@gmail.com>
2025-12-05 14:09:11 +0700

    Update Player.py

--- oangiapy/poker/core/Player.py
+++ oangiapy/poker/core/Player.py
@@ -2,13 +2,7 @@ from oangiapy.poker.core.Algorithm import Algorithm
 
 class Player:
     def __init__(self, cards, Algo: Algorithm):
-        self._algo = Algo()
-        self._cards = self._parse(cards)
-
-    def _parse(self, s: str):
-        arr = [self._algo.get_card_class()(x) for x in s.split(",")]
-        arr.sort(key=lambda c: (c.get_rank(), c.get_suit()))
-        return arr
+        self._algo = Algo(cards)
 
     def get_best_hands(self):
-        return self._algo.calc(self._cards)
+        return self._algo.get_best_hands()

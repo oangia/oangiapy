a7f5099bb34ab59c2b3ecc6c17c84cee23d6a92a
oangia <hqnhatdn@gmail.com>
2025-12-05 14:15:00 +0700

    Update Algorithm.py

--- oangiapy/poker/core/Algorithm.py
+++ oangiapy/poker/core/Algorithm.py
@@ -3,10 +3,12 @@ from oangiapy.poker.core.Hand import Hand
 from oangiapy.poker.core.Hands import Hands
 
 class Algorithm:
-    def __init__(self):
+    def __init__(self, cards):
         self._CardClass = Card
         self._HandClass = Hand
         self._HandsClass = Hands
+        self._cards = [self.get_card_class()(x) for x in cards.split(",")]
+        self._cards.sort(key=lambda c: (c.get_rank(), c.get_suit()))
 
     def get_card_class(self):
         return self._CardClass
@@ -17,7 +19,5 @@ class Algorithm:
     def get_hands_class(self):
         return self._HandsClass
 
-    def fast_scan(self, cards):
-        self._cards = cards
-
+    def fast_scan(self):
         return (False, None)

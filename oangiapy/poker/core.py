ad305f1d46b6625e6430c7b2977c68a6ac13d551
oangia <hqnhatdn@gmail.com>
2025-12-06 16:41:09 +0700

    Update core.py

--- oangiapy/poker/core.py
+++ oangiapy/poker/core.py
@@ -93,15 +93,35 @@ class Hands:
     def get_point(self):
         return self._point
 
+    def compare_point(self, other) {
+        if self.get_point() == other.get_point():
+            return 0
+        if self.get_point() > other.get_point():
+            return 1
+        return -1
+    }
+
+    def compare_dominance(self, other):
+        _back, _middle, _front = self.compare(other)
+        if _back == 0 and _middle ==0 and _front == 0
+            return 0
+
+        if _back >=0 and _middle >= 0 and _front >= 0:
+            return 1
+
+        if _back <=0 and _middle <= 0 and _front <= 0:
+            return -1
+
+        return 0
+    
+    def compare(self, other):
+        _back = self._back.compare(other.get_back())
+        _middle = self._middle.compare(other.get_middle())
+        _front = self._front.compare(other.get_front())
+        return [_back, _middle, _front]
+
     def __lt__(self, other):
-        _back = self._back.compare(other._back)
-        if _back == 0:
-            _middle = self._middle.compare(other._middle)
-            if _middle == 0:
-                _front = self._front.compare(other._front)
-                return _front == -1
-            return _middle == -1
-        return _back == -1
+        return self.compare_dominance(other) == -1
 
     def __gt__(self, other):
         return self._point > other._point

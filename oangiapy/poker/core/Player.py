0625c0cc61187ce37abc4c126a95787728f78f17
oangia <hqnhatdn@gmail.com>
2025-12-06 15:39:01 +0700

    Delete oangiapy/poker/core/Player.py

--- oangiapy/poker/core/Player.py
+++
@@ -1,8 +0,0 @@
-from oangiapy.poker.core.Algorithm import Algorithm
-
-class Player:
-    def __init__(self, cards, Algo: Algorithm):
-        self._algo = Algo(cards)
-
-    def get_best_hands(self):
-        return self._algo.get_best_hands()

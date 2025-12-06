b8e9c6553341e336f01e59d37929f8fbb24bab15
oangia <hqnhatdn@gmail.com>
2025-12-06 10:52:46 +0700

    Create Deck.py

---
+++ oangiapy/poker/core/Deck.py
@@ -0,0 +1,17 @@
+import random
+
+class Deck:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self._cards = [f"{r}{s}" for r in range(1, 14) for s in "scdh"]
+        random.shuffle(self._cards)
+
+    def draw(self, n):
+        if n > len(self._cards):
+            raise ValueError("not enough cards")
+        return [self._cards.pop() for _ in range(n)]
+
+    def __len__(self):
+        return len(self._cards)

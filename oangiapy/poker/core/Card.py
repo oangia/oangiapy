8a05dd67f068087646fa68ff3fae5d9524ebdc2d
oangia <hqnhatdn@gmail.com>
2025-12-11 14:31:51 +0700

    Create Card.py

---
+++ oangiapy/poker/core/Card.py
@@ -0,0 +1,23 @@
+class Card:
+    def __init__(self, name):
+        self._name = name
+        self._rank = int(name[:-1])
+        self._suit = name[-1]
+
+    def get_name(self):
+        return self._name
+        
+    def get_rank(self):
+        return self._rank
+
+    def get_suit(self):
+        return self._suit
+
+    def __eq__(self, other):
+        return self._name == other.get_name()
+
+    def __lt__(self, other):
+        return self._rank < other.get_rank()
+
+    def __repr__(self):
+        return self._name
